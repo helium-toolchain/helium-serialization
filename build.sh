aa55f0c6f4f29a6c9ca89b82a0329da34c480455
ExaInsanity <irisfeanora@outlook.com>
2022-09-20 14:47:52 +0200

    aand powershell pain

--- build.sh
+++ build.sh
@@ -1,3 +1,79 @@
+cc=gcc
+cppc=g++
+
+cflags=-std=c17 -O3 -fasm -fms-extensions
+cppflags=-std=c++17 -Ofast -fasm -fms-extensions
+
+build=./build
+obj=./obj
+
+c_castle_dir=./src/c/castle
+c_nbt_dir=./src/c/nbt
+
+cpp_castle_dir=./src/cpp/castle
+cpp_nbt_dir=./src/cpp/nbt
+
+c_obj_dir=./obj/c
+cpp_obj_dir=./obj_cpp
+
+function dotnet () {
+    dotnet pack -o $build -p:SymbolPackageFormat=snupkg --include-symbols --include-source --no-dependencies
+}
+
+function c () {
+    c_castle
+    c_nbt
+}
+
+function cpp () {
+    cpp_castle  
+    cpp_nbt
+}
+
+function dotnet_castle () {
+    dotnet pack -o $build src/csharp/Helium.Serialization.Castle -p:SymbolPackageFormat=snupkg --include-symbols --include-source --no-dependencies
+}
+
+function dotnet_nbt () {
+    dotnet pack -o $build src/csharp/Helium.Serialization.Castle -p:SymbolPackageFormat=snupkg --include-symbols --include-source --no-dependencies
+}
+
+function c_castle () {
+    $cc $cflags -c $c_castle_dir/index/inc.c -o $c_obj_dir/castle_index.o
+    # add all further components here
+    $cc -shared $c_obj_dir/castle_index.o $build/libheliumccastle.so
+}
+
+function c_nbt () {
+    $cc $cflags -c $c_nbt_dir/index/inc.c -o $c_obj_dir/nbt_index.o 
+    # add all further components here
+    $cc -shared $c_obj_dir/nbt_index.o $build/libheliumcnbt.so
+}
+
+function cpp_castle () {
+    $cppc $cppflags -c $cpp_castle_dir/index/inc.c -o $cpp_obj_dir/castle_index.o
+    # add all further components here
+    $cc -shared $cpp_obj_dir/castle_index.o $build/libheliumcppcastle.so
+}
+
+function cpp_nbt () {
+    $cppc $cppflags -c $cpp_nbt_dir/index/inc.c -o $cpp_obj_dir/castle_index.obj
+    # add all further components here
+    $cc -shared $cpp_obj_dir/nbt_index.o %build/libheliumcppnbt.so
+}
+
+function clean () {
+    rm -rf $build $obj
+}
+
+function setup () {
+    mkdir -p $build
+    mkdir -p $c_obj_dir
+    mkdir -p $cpp_obj_dir
+}
+
+# input parsing
+
 if [[ $# == 0 ]]; then
     dotnet
     c
@@ -70,77 +146,3 @@ fi
 if [[ $1 == "setup" ]]; then
     setup
 fi
-
-cc=gcc
-cppc=g++
-
-cflags=-std=c17 -O3 -fasm -fms-extensions
-cppflags=-std=c++17 -Ofast -fasm -fms-extensions
-
-build=./build
-obj=./obj
-
-c_castle_dir=./src/c/castle
-c_nbt_dir=./src/c/nbt
-
-cpp_castle_dir=./src/cpp/castle
-cpp_nbt_dir=./src/cpp/nbt
-
-c_obj_dir=./obj/c
-cpp_obj_dir=./obj_cpp
-
-function dotnet () {
-    dotnet pack -o build -p:SymbolPackageFormat=snupkg --include-symbols --include-source --no-dependencies
-}
-
-function c () {
-    c_castle
-    c_nbt
-}
-
-function cpp () {
-    cpp_castle  
-    cpp_nbt
-}
-
-function dotnet_castle () {
-    dotnet pack -o $build src/csharp/Helium.Serialization.Castle -p:SymbolPackageFormat=snupkg --include-symbols --include-source --no-dependencies
-}
-
-function dotnet_nbt () {
-    dotnet pack -o $build src/csharp/Helium.Serialization.Castle -p:SymbolPackageFormat=snupkg --include-symbols --include-source --no-dependencies
-}
-
-function c_castle () {
-    $cc $cflags -c $c_castle_dir/index/inc.c -o $c_obj_dir/castle_index.o
-    # add all further components here
-    $cc -shared $c_obj_dir/castle_index.o $build/libheliumccastle.so
-}
-
-function c_nbt () {
-    $cc $cflags -c $c_nbt_dir/index/inc.c -o $c_obj_dir/nbt_index.o 
-    # add all further components here
-    $cc -shared $c_obj_dir/nbt_index.o $build/libheliumcnbt.so
-}
-
-function cpp_castle () {
-    $cppc $cppflags -c $cpp_castle_dir/index/inc.c -o $cpp_obj_dir/castle_index.o
-    # add all further components here
-    $cc -shared $cpp_obj_dir/castle_index.o $build/libheliumcppcastle.so
-}
-
-function cpp_nbt () {
-    $cppc $cppflags -c $cpp_nbt_dir/index/inc.c -o $cpp_obj_dir/castle_index.obj
-    # add all further components here
-    $cc -shared $cpp_obj_dir/nbt_index.o %build/libheliumcppnbt.so
-}
-
-function clean () {
-    rm -rf $build $obj
-}
-
-function setup () {
-    mkdir -p $build
-    mkdir -p $c_obj_dir
-    mkdir -p $cpp_obj_dir
-}
